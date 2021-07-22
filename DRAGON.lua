0ebba3497ca92adb7a094199e02881cd0c46c0ad
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-07-22 08:40:56 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -1119,7 +1119,7 @@ end
 
 if text == 'تحديث السورس ' and DevSoFi(msg) then 
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/jokerrap12/hogan/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,'🧞‍♂️ تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -2502,9 +2502,9 @@ end
 return false
 end
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/jokerrap12/hogan/main/DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/jokerrap12/hogan/main/library')
-os.execute('wget https://raw.githubusercontent.com/jokerrap12/hogan/main/File_Bot')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/library')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot')
 send(msg.chat_id_, msg.id_,' 🧞‍♂️ تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -4870,7 +4870,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSoFi(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/jokerrap12/hogan/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/mostafanotbody/hogan9/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -4908,7 +4908,7 @@ t = " 🧞‍♂️ الملف ← "..file.."\n 🧞‍♂️ تم تعطيل م
 else
 t = " 🧞‍♂️ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/jokerrap12/hogan/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -4928,7 +4928,7 @@ t = " 🧞‍♂️ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " 🧞‍♂️ الملف ← "..file.."\n 🧞‍♂️ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/jokerrap12/hogan/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
