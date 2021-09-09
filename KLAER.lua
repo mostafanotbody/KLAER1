8cd843ae878ac55db6f5a4e82f2cb175df208f2d
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-09-09 22:38:24 +0200

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -1693,9 +1693,6 @@ local keyboard = {
 {'الغاء'},
 }
 send_inline_key(msg.chat_id_,bl,keyboard) 
-}  
-local msg_id = msg.id_/2097152/0.5  
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/xxxcccvvbbnn/911&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 end
 
