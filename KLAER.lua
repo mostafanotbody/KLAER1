0cb6a195264ba7361aebd4b6f3ad31fd81958750
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-10-04 15:22:38 +0200

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -2103,13 +2103,30 @@ end
 send(msg.chat_id_, msg.id_, t)
 end
 
-
 if text == 'الاحصائيات' and msa3d(msg) then 
+local Namebot = (bot_data:get(ban_id..'Name:Bot') or 'كلير') 
 local Groups = bot_data:scard(ban_id..'Chek:Groups')  
 local Users = bot_data:scard(ban_id..'User_Bot')  
-Text = ' ♲ الاحصائيات  \n'..' ♲ عدد الجروبات  ⇐ {'..Groups..'}'..'\n ♲  عدد المشتركين  ⇐ {'..Users..'}'
-send(msg.chat_id_, msg.id_,Text) 
-return false
+local getbioY = getbio(msg.sender_user_id_)
+Namebot = "NAME BOT ⇐"..Namebot..'\n⇐عـدد الجروبات هو ⇐ {'..Groups..'}\nعدد المشتركين هو ⇐ {'..Users..'}'
+local msg_id = msg.id_/2097152/0.5  
+keyboard = {} 
+keyboard.inline_keyboard = {
+{
+{text = 'SOURCE KLAER', url = "https://t.me/sasa_boody"},
+},
+{
+{text = 'اضغط لاضافه البوت لمجموعتك ' ,url="t.me/"..dofile("./Info.lua").botUserName.."?startgroup=start"},
+},
+}
+local function getpro(extra, result, success) 
+if result.photos_[0] then 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&caption=' .. URL.escape(Namebot).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+else 
+send(msg.chat_id_, msg.id_,Namebot, 1, 'md') 
+end 
+end 
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = ban_id, offset_ = 0, limit_ = 1 }, getpro, nil) 
 end
 if text == 'المشتركين' and msa3d(msg) then 
 local Groups = bot_data:scard(ban_id..'Chek:Groups')  
@@ -14396,14 +14413,30 @@ return false
 end
 end
 
-if text == 'الاحصائيات' then
-if Sudo(msg) then 
+if text == 'الاحصائيات' and msa3d(msg) then 
+local Namebot = (bot_data:get(ban_id..'Name:Bot') or 'كلير') 
 local Groups = bot_data:scard(ban_id..'Chek:Groups')  
 local Users = bot_data:scard(ban_id..'User_Bot')  
-Text = ' ♲ الاحصائيات  \n'..' ♲ عدد الجروبات  ⇐ {'..Groups..'}'..'\n ♲  عدد المشتركين  ⇐ {'..Users..'}'
-send(msg.chat_id_, msg.id_,Text) 
-end
-return false
+local getbioY = getbio(msg.sender_user_id_)
+Namebot = "NAME BOT ⇐"..Namebot..'\n⇐عـدد الجروبات هو ⇐ {'..Groups..'}\nعدد المشتركين هو ⇐ {'..Users..'}\nالبايو ⇐ {'..getbioY..'}'
+local msg_id = msg.id_/2097152/0.5  
+keyboard = {} 
+keyboard.inline_keyboard = {
+{
+{text = 'SOURCE KLAER', url = "https://t.me/sasa_boody"},
+},
+{
+{text = 'اضغط لاضافه البوت لمجموعتك ' ,url="t.me/"..dofile("./Info.lua").botUserName.."?startgroup=start"},
+},
+}
+local function getpro(extra, result, success) 
+if result.photos_[0] then 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&caption=' .. URL.escape(Namebot).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+else 
+send(msg.chat_id_, msg.id_,Namebot, 1, 'md') 
+end 
+end 
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = ban_id, offset_ = 0, limit_ = 1 }, getpro, nil) 
 end
 if text == 'الجروبات' then
 if Sudo(msg) then 
