687a79a5131bbfa7ef0764eb2af6674b942b1469
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-10-05 22:35:24 +0200

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -2102,30 +2102,6 @@ t = " ♲ لا يوجد مطورين ثانويين"
 end
 send(msg.chat_id_, msg.id_, t)
 end
-
-if text == 'الاحصائيات' and msa3d(msg) then 
-local Namebot = (bot_data:get(ban_id..'Name:Bot') or 'كلير') 
-local Groups = bot_data:scard(ban_id..'Chek:Groups')  
-local Users = bot_data:scard(ban_id..'User_Bot')  
-local getbioY = getbio(msg.sender_user_id_)
-local msg_id = msg.id_/2097152/0.5  
-local tessttm = '{'..Groups..'}'
-keyboard = {} 
-keyboard.inline_keyboard = {
-{
-{text = tessttm, url = "https://t.me/sasa_boody"},
-},
-{
-{text = 'اضغط لاضافه البوت لمجموعتك ' ,url="t.me/"..dofile("./Info.lua").botUserName.."?startgroup=start"},
-},
-}
-local function getpro(extra, result, success) 
-if result.photos_[0] then 
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&caption=' .. URL.escape(Namebot).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-end 
-end 
-tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = ban_id, offset_ = 0, limit_ = 1 }, getpro, nil) 
-end
 if text == 'المشتركين' and msa3d(msg) then 
 local Groups = bot_data:scard(ban_id..'Chek:Groups')  
 local Users = bot_data:scard(ban_id..'User_Bot')  
