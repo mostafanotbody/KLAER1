b1fb676887defe293cb459aec4e44d20efec9d88
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-10-12 07:44:58 -0500

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -5674,10 +5674,23 @@ local ban = ' ♲ عدد الادمنيه : '..data.administrator_count_..
 '\n\n ♲ عدد الاعضاء : '..data.member_count_..
 '\n\n ♲ عدد رسائل الجروب : '..(msg.id_/2097152/0.5)..
 '\n\n ♲  اسم الجروب : ['..ta.title_..']'
-send(msg.chat_id_, msg.id_, ban) 
-end,nil)
-end,nil)
+local msg_id = msg.id_/2097152/0.5  
+local sasaklaer = '\n\n ♲ عدد المطرودين'..data.kicked_count_..'
+keyboard = {} 
+keyboard.inline_keyboard = {
+{
+{text = sasaklaer, url = "https://t.me/sasa_boody"},
+},
+}
+local function getpro(extra, result, success) 
+if result.photos_[0] then 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&photo=' .. URL.escape(Namebot).."&photo="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+else 
+send(msg.chat_id_, msg.id_,Namebot, 1, 'md') 
 end 
+end 
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = ban_id, offset_ = 0, limit_ = 1 }, getpro, nil) 
+end
 
 if text and text:match("^صيح (.*)$") then
 local username = text:match("^صيح (.*)$") 
