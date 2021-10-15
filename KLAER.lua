5ecc6289b6cd8af39be1c511a3ab3e170078dffc
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-10-15 02:47:25 +0200

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -14367,13 +14367,17 @@ return false
 end
 end
 if text == 'ايدي' and msa3d(msg) then 
-local Namebot = (bot_data:get(ban_id..'Name:Bot') or 'كلير') 
+if result.username_ then
+username = '@'..result.username_ 
+else
+username = 'لا يوجد '
+end
 local rtp = Rutba(msg.sender_user_id_,msg.chat_id_)
 local getbioY = getbio(msg.sender_user_id_)
 local msg_id = msg.id_/2097152/0.5  
-local BBiduser = '• 🖤 | 𝙸𝙳 :  '..iduser..'
-local BBgetbioY = '• 🖤 | 𝙱𝙸𝙾 : '..getbioY..'
-local BBrtp = '• 🖤 | 𝚂𝚃𝙰 : '..rtp..'
+local BBusername = '• 🖤 | 𝑼𝑬𝑺 : :  '..username..''
+local BBgetbioY = '• 🖤 | 𝙱𝙸𝙾 : '..getbioY..''
+local BBrtp = '• 🖤 | 𝚂𝚃𝙰 : '..rtp..''
 keyboard = {} 
 keyboard.inline_keyboard = {
 {
@@ -14392,8 +14396,8 @@ keyboard.inline_keyboard = {
 local function getpro(extra, result, success) 
 if result.photos_[0] then 
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&photo=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-end
 end 
+end
 tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
 end
 
