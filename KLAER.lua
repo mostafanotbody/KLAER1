52e0247962d02560c9e817afb719189f098c2351
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-12-05 20:38:34 +0200

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -14463,7 +14463,6 @@ username = result.username_
 else
 username = 'sasa_boody'
 end
-local msg_id = msg.id_/2097152/0.5  
 local textt = ' 🌝🖤 رتبتك في البوت » '..Rutba(msg.sender_user_id_,msg.chat_id_)
 local Sasa = 'https://t.me/xxxcccvvbbnn/903'
 keyboard = {} 
@@ -14472,14 +14471,8 @@ keyboard.inline_keyboard = {
 {text = textt, url="http://t.me/"..username},
 },
 }
-local function getpro(extra, result, success) 
-if result.photos_[0] then 
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&photo=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-else 
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=' .. URL.escape(Sasa).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-end end 
-tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
-end,nil)
+local msg_id = msg.id_/2097152/0.5
+https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 
 if text == 'تفعيل رتبتي' and Manager(msg) then   
