b562424fb8a8cd22a7d752c8deac7ab5d842dc2e
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-12-06 04:09:06 +0200

    Add files via upload

--- KLAER.lua
+++ KLAER.lua
@@ -14510,9 +14510,7 @@ local msg_id = msg.id_/2097152/0.5
 local textt = '.   ◍ '..Rutba(msg.sender_user_id_,msg.chat_id_)
 keyboard = {} 
 keyboard.inline_keyboard = {
-{
-{text = textt, url="http://t.me/"..username},
-},
+{{text = textt, url="http://t.me/"..username}},
 }
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end,nil)
@@ -19005,7 +19003,7 @@ keyboard.inline_keyboard = {
 {text = '• الاسد ،🦁', callback_data="/zking"},{text = '• الثور 🐂', callback_data="/zzor"},
 },
 {
-{text = '• العقرب 🦂', callback_data="/zakrb"},{text = '• القوس 🏹', callback_data="/zkos"},
+{text = '• العقرب ??', callback_data="/zakrb"},{text = '• القوس 🏹', callback_data="/zkos"},
 },
 {
 {text = '• الحمل 🐐', callback_data="/zBoody"},
